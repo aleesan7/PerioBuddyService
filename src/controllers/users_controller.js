e57d19bf1d2d8e2630c159b568aaa4b579d0901c
aleesan7 <ka_sanchezsamayoa@outlook.com>
2024-04-16 23:58:38 -0600

    Modificando endpoint de registro y agregando endpoint de actualizar informacion de usuario.

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -40,8 +40,11 @@ const register = (req, res, next) => {
 
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
+    var birthdate = req.body.birthdate;
+    var gender = req.body.gender;
     var email = req.body.email;
     var pass = req.body.password;
+    var urlProfPic = req.body.urlProfPic;
 
     db.query(
         `SELECT * FROM Users WHERE email = '${email}'`,
@@ -57,7 +60,7 @@ const register = (req, res, next) => {
             let hash = crypto.createHash('md5').update(pass).digest("hex");
 
             db.query(
-                `INSERT INTO Users (firstName, lastName, email, password, dateCreated) VALUES ('${firstName}', '${lastName}', '${email}', '${hash}', NOW())`,
+                `INSERT INTO Users (firstName, lastName, birthdate, gender, email, password, dateCreated, urlProfPic) VALUES ('${firstName}', '${lastName}', '${birthdate}', '${gender}', '${email}', '${hash}', NOW(), '${urlProfPic}')`,
                 (err, result) => {
                 if (err) {
                     return res.status(400).send({
@@ -75,8 +78,56 @@ const register = (req, res, next) => {
     );
 }
 
+const updateInfo = async (req, res) => {
+
+    var { firstName, lastName, birthdate, gender, email } = req.body;
+  
+    /*var folderPath = `Fotos_Perfil/${nombreFoto}.jpg`;
+  
+    // Se convierte la imagen a un buffer
+    const buff = Buffer.from(foto, 'base64');
+  
+    // Se colocal la region del bucket y las credenciales
+    // Nunca subir las credecioanes a github o gitlab
+    // posiblemete les puede bloquar la cuenta de aws por que expusieron las credenciales en un repositorio publico
+    aws.config.update({
+      accessKeyId: 'AKIA3WBJ23QODDJJOXND',
+      secretAccessKey: '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',
+      region:  'us-east-1'
+    }); 
+  
+    // Se crea una valirable que contiene el servicio o caracteristicas S3
+    const s3 = new aws.S3();
+    
+    const paramsS3 = {
+      Bucket     : 'practica1-g7-imagenes',
+      Key        : folderPath,
+      Body       : buff,
+      ContentType: 'image'
+    }
+  
+    const s3Resp = await s3.upload(paramsS3).promise();
+    console.log(s3Resp.Location);*/
+  
+    
+    db.query(
+      `UPDATE Users SET firstName = '${firstName}', lastName = '${lastName}', birthDate = '${birthdate}', gender = '${gender}' WHERE email = '${email}'`,
+      (err, result) => {
+      if (err) {
+          return res.status(400).send({
+          msg: err
+          });
+      }
+      }
+    );
+    
+    return res.status(200).json({ message: 'Usuario actualizado exitosamente' }); 
+  
+  }
+
 
 module.exports = {
     login : login,
-    register : register
+    register : register,
+    updateInfo : updateInfo
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,6 @@ const usersController = require('../controllers/users_controller')
 
 router.post('/users/login', usersController.login);
 router.post('/users/register', usersController.register);
+router.post('/users/updateInfo', usersController.updateInfo);
 
 module.exports = router;
